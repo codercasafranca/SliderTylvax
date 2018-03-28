3f29218daae38de698e67250403fa1f4bc930b1c
Alexander <codercasafranca@gmail.com>
2018-03-28 17:22:41 -0500

    Iniciamos

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $(".owl-carousel.tylvax").owlCarousel({  	
+    margin:10,
+    responsiveClass:true,
+    nav:true,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    dots:true,
+    loop:true,
+    responsive:{
+        0:{
+            items:1,           
+        },
+        600:{
+            items:1,           
+        },
+        1000:{
+            items:1,                
+        }
+    }
+  });
+});
